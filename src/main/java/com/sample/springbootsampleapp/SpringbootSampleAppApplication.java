7c10388aa5d85b08f226ce131bdf4e042eb6457a
KYEONGMIN CHO <ggingmin@gmail.com>
2021-08-29 00:00:22 +0900

    commit

---
+++ src/main/java/com/sample/springbootsampleapp/SpringbootSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.springbootsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSampleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSampleAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sample/springbootsampleapp/SpringbootSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.springbootsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
